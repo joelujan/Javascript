18c2368c6888c7af70a551b2e54b2d128b43fcab
Joel Lujan <joelandreslujan@gmail.com>
2022-10-03 21:39:59 -0300

    feature: Adding some examples for Javascript

---
+++ leccion01/01-01-holamundo.js
@@ -0,0 +1,8 @@
+/*
+''' Javascript no tiene capacidad multihilo o multiples procesos simultáneos, lee y ejecuta de arriba hacia abajo
+''' ; se utiliza para cerrar 
+*/
+var nombre = "Ariel";
+nombre = "Alberto \nAriel\n";
+var apellido = "Betancud";
+console.log(nombre + apellido);
---
+++ leccion02/02-01-TiposDatos.js
@@ -0,0 +1,18 @@
+//Tipos de datos en JavasCript
+/*
+La sintaxis en lo que es comentarios es muy
+similar a la de Java realmente diriamos que es identica
+*/
+var nombre = 'Ariel'; //Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo Numérico
+console.log(numero);
+
+var objeto = {
+    nombre : "Ariel",
+    apellido : "Betancud",
+    telefono : "2614568788"
+}
+
+console.log(objeto);
