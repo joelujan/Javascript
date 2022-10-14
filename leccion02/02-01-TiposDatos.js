55d5f3c69590d0bdb20cafb26d983b68921bb8ed
Joel Lujan <lujan.joel@hotmail.com>
2022-10-14 19:13:22 -0300

    feat: Adding new classes of Javascritp
    Hasta variables undefined

--- leccion02/02-01-TiposDatos.js
+++ leccion02/02-01-TiposDatos.js
@@ -4,11 +4,17 @@ La sintaxis en lo que es comentarios es muy
 similar a la de Java realmente diriamos que es identica
 */
 var nombre = 'Ariel'; //Tipo Str
-console.log(nombre);
+console.log(typeof nombre);
 
-var numero = 3000; //Tipo Numérico
+nombre = 7;
+console.log(typeof nombre);
+
+nombre = 12.3;
+console.log(typeof nombre);
+//Tipo Numérico
+var numero = 3000; 
 console.log(numero);
-
+//Tipo Objeto
 var objeto = {
     nombre : "Ariel",
     apellido : "Betancud",
@@ -16,3 +22,35 @@ var objeto = {
 }
 
 console.log(objeto);
+
+//Tipo de dato boolean
+var bandera = true;
+console.log(typeof bandera);
+
+//Funciones tipo de dato; nos permite reutilizar lineas de codigo
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(simbolo)
+
+//Tipo de dato clase, es una función en Javascript
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre; //es para hacer referencia a los atributos de nuestra clase
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
+
+console.log(typeof Persona);
+
+//Tipo de dato undefined, cualquier tipo de variable que no haya sido inicializada
+var x;
+console.log(typeof x);
+x = undefined;
+console.log(typeof x);
+//null : significa ausencia de valor
+var y = null; //null no es un tipo de dato, pero su origen es de tipo object
+console.log(typeof y);
