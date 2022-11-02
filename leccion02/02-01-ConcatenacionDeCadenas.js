659e3ca437d556b245b50b04e3de16631025f0fb
Joel Lujan <lujan.joel@hotmail.com>
2022-11-01 22:36:37 -0300

    feat: new Lesson of Javascript

--- leccion02/02-01-ConcatenacionDeCadenas.js
+++ leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,23 @@ console.log(juntos);// CONTEXTO STRING O CONTEXTO CADENA
 
 nombre += apellido; // Tercera Concatenación usando el operador simplificado
 console.log(nombre);
+
+//Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; Una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y;//Se pueden declarar, o crear varias variables dentro de una misma linea
+x = 17, y=21; //Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; //se asigna el valor de la asignacion
+console.log(z) //Case sensitive, son importante las mayusculas
+
+
+let _1num = 31; //no utilizar numeros para iniciar el nombre de una variable
+let rompiendo = "rompe";//No se pueden utilizar palabras reservadas
+
+console.log(_1num)
+console.log(rompiendo)
