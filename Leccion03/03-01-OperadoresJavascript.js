0caf6deb43b7b7fddf0f1b5a027b06f7431fa1e9
Joel Lujan <lujan.joel@hotmail.com>
2022-11-14 21:35:36 -0300

    feat:adding class to Javascript

---
+++ Leccion03/03-01-OperadoresJavascript.js
@@ -0,0 +1,14 @@
+//Ejercicio para encontrar numeros pares e impares
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un numero par");
+}else{
+    console.log("Es un numero impar");
+}
+//Ejercicio: es mayor de edad
+let edad = 18, adulto=18;
+if (edad >= adulto){
+    console.log("Usted es una persona adulta");
+}else{
+    console.log("Usted es una persona menor de edad");
+}
